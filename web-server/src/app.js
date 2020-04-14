f60bc20e13ba59980dee47c5ecf7c2dd3e1d06eb
Nacool <nagariya23nakul@gmail.com>
2020-04-14 23:10:10 +0530

    web server done
    web server is created, serving files using express, used handlebars hbs

---
+++ web-server/src/app.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+// Define path for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Home Page",
+    author: "Nakul Nagariya",
+    name: "Nakul Nagariya",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Page",
+    src: "/img/DSC_0328.JPG",
+    name: "Nakul Nagariya",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Nakul Nagariya",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forcast: "its snowing outside",
+    location: "bengaluru",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Your server started on port 3000");
+});
